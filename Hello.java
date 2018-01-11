e7b9f173a454d61a45577f3a652037c11ef06d6d
Bizz Michelson <bizz.michelson@gmail.com>
2018-01-11 13:59:45 -0600

    declaring variables

--- Hello.java
+++ Hello.java
@@ -5,9 +5,16 @@
 public class Hello {
 
     public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
         // generate some simple output
         //System.out.println("Hello, World!");
-        System.out.print("Hello!\nHow are you doing?\n");
+        //System.out.print("Hello!\nHow are you doing?\n");
     }
 }
 
