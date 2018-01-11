a506235a139573f9915ec0b062a45a7007c27835
Bizz Michelson <bizz.michelson@gmail.com>
2018-01-11 14:26:44 -0600

    practice with variables

--- Time.java
+++ Time.java
@@ -2,16 +2,20 @@
 
 
   
-public class Hello {
+public class Time {
 
     public static void main(String[] args) {
         int hour = 11;
         int minute = 59;
+        int second = 05;
         System.out.print("The current time is ");
         System.out.print(hour);
         System.out.print(":");
         System.out.print(minute);
         System.out.println(".");
+        
+        System.out.print("Number of minutes since midnight: ");
+        System.out.println(hour * 60 + minute);
         // generate some simple output
         //System.out.println("Hello, World!");
         //System.out.print("Hello!\nHow are you doing?\n");
